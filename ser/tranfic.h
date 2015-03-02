8580a55f0601ffade89dc42d2baa29e654d88243
1796843639@qq.com <Striver@localhost.localdomain>
2015-03-02 15:21:09 +0800

    服务端和客户端的公用头文件

---
+++ ser/tranfic.h
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  tranfic.h
+ *
+ *    Description:  客户端和服务端公用的头文件
+ *
+ *        Version:  1.0
+ *        Created:  2013年08月13日 10时49分14秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  cuijiaojiao, cuijiaojiaofendou@gmail.com
+ *        Company:  Class 1204 of Software
+ *
+ * =====================================================================================
+ */
+
+#ifndef CUI
+#define CUI
+
+    #include <unistd.h>
+    #include <assert.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <string.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <errno.h>
+    #include <dirent.h>
+    #include <sys/stat.h>
+    #include <fcntl.h>
+
+    #define BUFSIZE   1024
+   
+#endif
+
+
